58153bd9f6903d056a149e6fc72d316bd93a8e76
Steve <stvartavia@gmail.com>
2024-07-18 15:17:06 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -3,13 +3,21 @@ let userInput = document.getElementById('user-input');
 let sendBtn = document.getElementById('send-btn');
 
 // array that will contain all the data for the chatbot to display for users
-const data = [
+const saludo = [
     'Hola',
     'Como estas?',
     'Encantando de verte!',
     'Me alegra verte!',
     'Como puedo ayudarte?'
 ];
+
+const comoEstas = [
+    'Muy bien, como te ayudo',
+    'Me encuentro muy bien',
+    'Estoy excelentemente bien',
+    'Estoy fantasticamente bien'
+]
+
 
 let output = '';
 
@@ -30,8 +38,18 @@ const interaction = () => {
     }
 
     if(input === 'hola'){
-        let index = Math.floor(Math.random() * data.length);
-        let response = data[index];
+        let index = Math.floor(Math.random() * saludo.length);
+        let response = saludo[index];
+        let botMessage = document.createElement('p');
+        botMessage.classList.add('botMessage');
+        botMessage.textContent = response;
+        chatBox.appendChild(botMessage);
+
+    }
+
+    if(input.includes('como estas')){
+        let index = Math.floor(Math.random() * comoEstas.length);
+        let response = comoEstas[index];
         let botMessage = document.createElement('p');
         botMessage.classList.add('botMessage');
         botMessage.textContent = response;
