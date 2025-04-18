4feaad299ef37bceb402dc0f6149e2d445b002cd
Steve Artavia <stvartavia@gmail.com>
2025-04-18 13:59:06 -0600

    development alert

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import { handleChatInteraction } from './chatLogic.js';
 
+alert("I'm still developing this project, it's not finished");
+
 let chatBox = document.getElementById('chat-box');
 let userInput = document.getElementById('user-input');
 let sendBtn = document.getElementById('send-btn');
